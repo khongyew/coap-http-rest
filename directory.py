26ac8d5f6616303da4f663da68367a2729c77625
khongyew <khongyew@gmail.com>
2019-01-07 15:51:45 +0800

    10.0.0.3:5000/0 returns  a temperature reading from sensor id 0

--- directory.py
+++ directory.py
@@ -5,6 +5,8 @@
 import urllib.request
 from parser import BorderRouterHtmlParser
 from json import *
+import asyncio
+from aiocoap import *
 
 # content = urllib.request.urlopen('http://[fd00::212:4b00:812:4]').read()
 # content = content.decode('UTF-8')
@@ -21,7 +23,6 @@ class Directory:
 
     def __init__(self):
         self.list_of_sensors = []
-        pass
 
     def parse(self):
         html_content = urllib.request.urlopen(self.BORDER_ROUTER_URL).read()
@@ -33,15 +34,25 @@ class Directory:
         # get the list of sensor tag ip addresses
         list_of_ip_addr = html_parser.get_sensor_ip_addr()
 
-        # build the list of dictionaries of sensor tag
-        # for example:
-        # list_of_sensors = [{'id': '0', 'ip_address' : 'fd00::212:4b00:b01:7a86'},
-        #                    {'id': '1', 'ip_address' : 'fd00::212:4b00:b00:9202'}]
+        # build the list of sensor instances
         id = 0
         for ip_address in list_of_ip_addr:
             sensor = SensorTag(id, ip_address)
-            self.list_of_sensors.append(sensor.get_dict())
+            self.list_of_sensors.append(sensor)
             id = id + 1
+
+    # returns a dictionary representation of the directory
+    def get_dict(self):
+        list_of_dict_of_sensors = []
+
+        # build the list of dictionaries of sensor tag
+        # for example:
+        # list_of_dict_of_sensors = [{'id': '0', 'ip_address' : 'fd00::212:4b00:b01:7a86'},
+        #                            {'id': '1', 'ip_address' : 'fd00::212:4b00:b00:9202'}]
+
+        for sensor in self.list_of_sensors:
+            list_of_dict_of_sensors.append(sensor.get_dict())
+        return {'available sensors' : list_of_dict_of_sensors}
 
     def update(self):
         self.parse()
@@ -63,3 +74,19 @@ class SensorTag:
             'status': self.status
             }
         return json_dict
+    
+    async def get_temp(self):
+        #Uses CoAP client to get temperature readings from a sensor tag
+        coap_client = await Context.create_client_context()
+        my_uri = 'coap://' + '[' + self.ip_address + ']' + '/sen/tmp/amb'
+        # 'coap://[fd00::212:4b00:b01:7a86]/sen/tmp/amb'
+        my_request = Message(code=GET, uri=my_uri)
+
+        try:
+            response_from_server = await coap_client.request(my_request).response
+        except Exception as e:
+            print('Failed to fetch resource:')
+            print(e)
+        else:
+            # print('Result: %s\n%r'%(response.code, response.payload))
+            return response_from_server.payload.decode('UTF-8')
--- main.py
+++ main.py
@@ -2,28 +2,40 @@
 
 from flask import Flask
 from flask_restful import Api, Resource
-from directory import Directory
+from directory import Directory, SensorTag
 
 app = Flask(__name__)
 api = Api(app)
 
+# Query the directory to get the list of sensors 
+# (which are dictionaries containing info such as id, ip_address, status)
+directory = Directory()
+directory.update()
+
 # Classes for handling HTTP requests
 class Home(Resource):
     def get(self):
-        return {'available sensors' : list_of_sensors}
+        return directory.get_dict()
 
 class Test(Resource):
     def get(self):
         list_of_test = [{'test' : '0'}, {'test' : '1'}, {'test' : '2'}]
         return {'list of test' : list_of_test}
 
-# Query the directory to get the list of sensors 
-# (which are dictionaries containing info such as id, ip_address, status)
-directory = Directory()
-directory.update()
-list_of_sensors = directory.get_list_of_sensors()
+class SensorQuery(Resource):
+    import asyncio
+    asyncio_event_loop = asyncio.get_event_loop()
+
+    def get(self, sensor_id):
+        print('sensor id = %d' % sensor_id)
+        sensor = directory.get_list_of_sensors()[sensor_id]
+        # return sensor.get_dict()
+        ret = self.asyncio_event_loop.run_until_complete(sensor.get_temp())
+        return ret
+        
 
 # Initialising the HTTP REST API server
 api.add_resource(Home, '/')
 api.add_resource(Test, '/test')
+api.add_resource(SensorQuery, '/<int:sensor_id>')
 app.run(debug=True, host='10.0.0.3', port=5000)
