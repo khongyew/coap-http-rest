a8a41527635248fa4ccfebefb31032c55a297af5
khongyew <khongyew@gmail.com>
2019-02-06 19:07:37 +0800

    Some refinement

--- data_recorder.py
+++ data_recorder.py
@@ -5,10 +5,6 @@ import asyncio
 import datetime, threading, time
 import os, csv
 
-directory = Directory()
-directory.update()
-
-sensors = directory.get_list_of_sensors()
 
 # Readings to get:
 # light             10.0.0.3:5000/0/opt/light
@@ -18,7 +14,7 @@ sensors = directory.get_list_of_sensors()
 
 # Gets the readings from the Sensor Tag
 # Returns light, temperature, humidity as a tuple 
-def get_all_readings(sensor_id=None, sensor_instance=None):
+def get_all_readings(sensors=None, sensor_id=None, sensor_instance=None):
     # loop = asyncio.get_event_loop()
 
     # Code will be running outside of the main thread
@@ -28,8 +24,8 @@ def get_all_readings(sensor_id=None, sensor_instance=None):
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
 
-    if sensor_id is not None:
-        sensor = sensors[sensor_id]    
+    if sensors is not None and sensor_id is not None:
+        sensor = sensors[sensor_id]
 
     elif sensor_instance is not None:
         sensor = sensor_instance
@@ -51,30 +47,39 @@ def get_all_readings(sensor_id=None, sensor_instance=None):
 
     return light, temperature, humidty
 
-def periodic_record(filename, start_time=time.time()):
-    next_time = start_time + 300 # period
+def periodic_record(filename, sensors, period, start_time=time.time()):
+    next_time = start_time + period
     
     # Do processing here
     # ------------------- 
 
     # print(datetime.datetime.now())
 
-    record(filename)
+    record(filename, sensors)
 
     # ------------------
     # End of processing 
 
     interval = next_time - time.time()
-    threading.Timer(interval, periodic_record, args=[filename], kwargs={'start_time':next_time}).start()
 
-def record(filename):
+    t = threading.Timer(
+        interval, 
+        periodic_record, 
+        args=[filename, sensors, period], 
+        kwargs={'start_time':next_time}
+    )
+
+    t.start()
+
+def record(filename, sensors):
     timestamp = datetime.datetime.now()
 
     csvfile = open(filename , 'a')
     writer = csv.writer(csvfile)
 
-    # CSV column headings
-    writer.writerow(['timestamp', 'id', 'light', 'temperature', 'humidity'])
+    # Write CSV column headings if the file is empty (i.e new file)
+    if (os.stat(filename).st_size == 0):
+        writer.writerow(['timestamp', 'id', 'light', 'temperature', 'humidity'])
 
     for sensor in sensors:
 
@@ -98,19 +103,30 @@ def test():
         i += 1
 
     filename = 'sensor-data-%s.csv' % i
+    
+    directory = Directory()
+    directory.update()
+
+    sensors = directory.get_list_of_sensors()
 
     # for i in range(len(sensors)):
     #     light, temperature, humidity = get_all_readings(sensor_id = i)
     #     print(light, temperature, humidity)
 
-    record(filename)
+    record(filename, sensors)
 
-def start():
+def start(sensors):
     index = 0
     while os.path.exists('sensor-data-%s.csv' % index):
         index += 1
 
     filename = 'sensor-data-%s.csv' % index
-    periodic_record(filename)
+    periodic_record(filename, sensors, 10)
 
+if __name__ == "__main__":
+    directory = Directory()
+    directory.update()
 
+    sensors = directory.get_list_of_sensors()
+
+    start(sensors)
