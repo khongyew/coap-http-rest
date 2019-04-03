faa3b9b0b738e2a99eeaf5542f6a0ee7e99fa747
khongyew <khongyew@gmail.com>
2019-04-03 18:02:12 +0800

    Change clustering to periodic task

--- clustering.py
+++ clustering.py
@@ -7,6 +7,7 @@ import matplotlib.animation as animation
 from matplotlib import style
 
 import os, copy, json
+import datetime, threading, time
 
 # Plan:
 # 1. Read the latest csv file
@@ -22,65 +23,104 @@ def get_latest_file():
     filename = 'sensor-data-%s.csv' % (index-1)
     return open(filename, 'r')
 
-# Initialise sensor list for each sensor
-sensors_readings = []
-for i in range(NUM_OF_SENSORS):
-    sensors_readings.append([])
+# def init():
+#     # Initialise sensor list for each sensor
+#     for i in range(NUM_OF_SENSORS):
+#         sensors_readings.append([])
 
-# Read the latest CSV file
-csv_file = get_latest_file()
-csv_file.readline() # Skip the first line
+def clustering_task():
+    sensors_readings = []
 
-# Parse the CSV file and split the data into
-# sensors_readings[0], sensors_readings[1], sensors_readings[2]
-for line in csv_file:
-    line = line.split(',')
+    # Initialise sensor list for each sensor
+    for i in range(NUM_OF_SENSORS):
+        sensors_readings.append([])
 
-    id = int(line[1])
-    light = float(line[2])
-    temperature = float(line[3])
-    humidity = float(line[4])
+    # Read the latest CSV file
+    csv_file = get_latest_file()
+    csv_file.readline() # Skip the first line
 
-    sensors_readings[id].append([temperature, humidity, light])
+    # Parse the CSV file and split the data into
+    # sensors_readings[0], sensors_readings[1], sensors_readings[2]
+    for line in csv_file:
+        line = line.split(',')
 
-csv_file.close()
+        id = int(line[1])
+        light = float(line[2])
+        temperature = float(line[3])
+        humidity = float(line[4])
 
-# Convert list to numpy array
-for i in range(NUM_OF_SENSORS):
-    sensors_readings[i] = np.array(sensors_readings[i])
+        sensors_readings[id].append([temperature, humidity, light])
 
-# KMeans Clustering
-kmeans = KMeans(n_clusters=2)
-labels = []
+    csv_file.close()
 
-# For each sensor, perform clustering and save the generated labels
-for i in range(NUM_OF_SENSORS):
-    kmeans.fit(sensors_readings[i])
-    labels.append(copy.deepcopy(kmeans.labels_))
+    # Convert list to numpy array
+    for i in range(NUM_OF_SENSORS):
+        sensors_readings[i] = np.array(sensors_readings[i])
 
-cluster_data = {}
+    # KMeans Clustering
+    kmeans = KMeans(n_clusters=2)
+    labels = []
 
-# Find the number of points in each cluster of each sensor
-for i in range(NUM_OF_SENSORS):
-    count_0 = 0
-    count_1 = 0
+    # For each sensor, perform clustering and save the generated labels
+    for i in range(NUM_OF_SENSORS):
+        kmeans.fit(sensors_readings[i])
+        labels.append(copy.deepcopy(kmeans.labels_))
 
-    for j in range(len(labels[i])):
-        if (labels[i][j] == 0):
-            count_0 = count_0 + 1
-        else:
-            count_1 = count_1 + 1
+    cluster_data = {}
 
-    cluster_data["sensor" + str(i)] = {"cluster0count" : count_0, "cluster1count": count_1}
+    # Find the number of points in each cluster of each sensor
+    for i in range(NUM_OF_SENSORS):
+        count_0 = 0
+        count_1 = 0
 
-# Convert cluster data to json
-json_string = json.dumps(cluster_data)
+        for j in range(len(labels[i])):
+            if (labels[i][j] == 0):
+                count_0 = count_0 + 1
+            else:
+                count_1 = count_1 + 1
 
-# write cluster data to file
-json_file = open("clustering.json", "wt")
-json_file.write(json_string)
-json_file.close()
+        cluster_data["sensor" + str(i)] = {"cluster0count" : count_0, "cluster1count": count_1}
 
+    # Convert cluster data to json
+    json_string = json.dumps(cluster_data)
+
+    # write cluster data to json file
+    json_file = open("clustering.json", "wt")
+    json_file.write(json_string)
+    json_file.close()
+
+def periodic_task(task, period, start_time=time.time()):
+    # Using threading.Timer() without drift:
+    # https://stackoverflow.com/a/18180189
+
+    next_time = start_time + period
+    
+    # Do processing here
+    # ------------------- 
+
+    print("Running at: " + str(datetime.datetime.now()))
+
+    task()
+
+    # ------------------
+    # End of processing 
+
+    interval = next_time - time.time()
+
+    t = threading.Timer(
+        interval, 
+        periodic_task, 
+        args=[task, period], 
+        kwargs={'start_time':next_time}
+    )
+
+    t.start()
+
+def main():
+    periodic_task(clustering_task, 120)
+
+if __name__ == "__main__":
+    main()
 
 #  Matplotlib Stuffs
 # --------------------------------------------------------------------------
