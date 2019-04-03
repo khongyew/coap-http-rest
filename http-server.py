72e158686ae3f3e4391e677ab20f069bf4c9c2a0
khongyew <khongyew@gmail.com>
2019-04-03 16:33:41 +0800

    Fixed asyncio issue. Added endpoint for clustering

--- http-server.py
+++ http-server.py
@@ -3,7 +3,7 @@
 from flask import Flask
 from flask_restful import Api, Resource
 from directory import Directory
-import asyncio
+import asyncio, json
 
 app = Flask(__name__)
 api = Api(app)
@@ -30,19 +30,23 @@ class SensorQuery(Resource):
     def get(self, device_id, sensor_chip, measurement):
 
         sensor = directory.get_list_of_sensors()[device_id]
-
-        # if (resource == 'temperature'):
-        #     value = self.asyncio_event_loop.run_until_complete(sensor.get_temperature())
-        #     unit = 'celsius'
-
-        # elif (resource == 'humidity'):
-        #     value = self.asyncio_event_loop.run_until_complete(sensor.get_humidity())
-        #     unit = '%RH'
         
+        # NOTE: run this flask server with the option "threaded=False" to prevent
+        # asyncio errors such as "event loop is already running" 
+
         resource = self.asyncio_event_loop.run_until_complete(
             sensor.get_resource(sensor_chip, measurement))
 
         return resource
+
+class Clustering(Resource):
+
+    def get(self):
+        json_file = open("./clustering.json", "rt")
+        json_string = json_file.read()
+        json_file.close()
+
+        return json.loads(json_string)
 
 # Start Data Recorder        
 # import data_recorder
@@ -53,4 +57,5 @@ class SensorQuery(Resource):
 api.add_resource(Home, '/')
 api.add_resource(Test, '/test')
 api.add_resource(SensorQuery, '/<int:device_id>/<string:sensor_chip>/<string:measurement>')
-app.run(debug=True, host='0.0.0.0', port=5000)
+api.add_resource(Clustering, '/analytics/clustering')
+app.run(debug=True, host='0.0.0.0', port=5000, threaded=False)
